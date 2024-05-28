a4f0db917db506271cf0278db6f85b505728a5ff
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-05-28 22:51:32 +0200

    wip

--- src/components/SettingsEdit.vue
+++ src/components/SettingsEdit.vue
@@ -20,6 +20,15 @@
                 </button>
             </div>
         </wwEditorFormRow>
+
+        <div v-if="plugin.project?.Name" class="p-2 ww-border-radius-02 border-primary">
+            <div class="body-sm content-secondary">
+                <span class="p-1 mr-2 content-primary ww-border-radius-01 bg-secondary bold">
+                    {{ plugin.project.Name }}
+                </span>
+                <span>{{ plugin.project.Subdomain }}</span>
+            </div>
+        </div>
     </div>
     <wwLoader :loading="isLoading" />
 </template>
