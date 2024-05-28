0d099df2050d241f16f915a01fb8cbae2a9b1e79
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-05-28 22:55:37 +0200

    wip

--- src/components/SettingsSummary.vue
+++ src/components/SettingsSummary.vue
@@ -1,12 +1,20 @@
 <template>
     <wwEditorFormRow label="Integration token">
-        <div class="flex items-center mb-2">
-            <div><wwEditorIcon large name="data" class="mr-2" /></div>
+        <div class="flex items-center mb-2 pt-2">
+            <div><wwEditorIcon large name="key" class="mr-2" /></div>
             <span class="truncate">{{
                 settings.privateData.integrationToken
                     ? '*****************************************'
                     : 'Integration token missing'
             }}</span>
+        </div>
+        <div v-if="plugin.project?.Name" class="p-2 ww-border-radius-02 border-primary">
+            <div class="body-sm content-secondary">
+                <span class="p-1 mr-2 content-primary ww-border-radius-01 bg-secondary bold">
+                    {{ plugin.project.Name }}
+                </span>
+                <span>{{ plugin.project.Subdomain }}</span>
+            </div>
         </div>
     </wwEditorFormRow>
 </template>
@@ -14,6 +22,7 @@
 <script>
 export default {
     props: {
+        plugin: { type: Object, required: true },
         settings: { type: Object, required: true },
     },
 };
