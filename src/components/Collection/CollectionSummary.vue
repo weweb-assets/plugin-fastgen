677e6b7a3d7e2e13cec89c32b3ad4ccbf872eb2c
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-05-29 11:42:45 +0200

    wip

--- src/components/Collection/CollectionSummary.vue
+++ src/components/Collection/CollectionSummary.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="supabase-collection-summary">
-        <div class="p-2 mb-4 ww-border-radius-02 border-primary">
-            {{ config.path }} <br />
-            <span class="body-sm content-secondary mt-1">{{ plugin.project.Subdomain + config.path }}</span>
+        <div class="p-2 ww-border-radius-02 border-primary">
+            <div class="body-sm content-secondary">
+                <div class="flex">
+                    <span class="p-1 mr-2 content-primary ww-border-radius-01 bg-secondary bold">
+                        {{ selectedRoute.Name }}
+                    </span>
+                    <span class="p-1 mr-2 content-primary ww-border-radius-01 bg-secondary bold">
+                        {{ selectedRoute.Method }}
+                    </span>
+                </div>
+                <span class="body-sm content-secondary mt-1">{{ plugin.project.Subdomain + config.path }}</span>
+            </div>
         </div>
     </div>
 </template>
@@ -12,6 +21,11 @@ export default {
     props: {
         config: { type: Object, required: true },
         plugin: { type: Object, required: true },
+    },
+    computed: {
+        selectedRoute() {
+            return this.plugin.routes.find(route => route.Path === this.config.path);
+        },
     },
 };
 </script>
