319c236ec8c6008c07531dd4cf61ff92873b1579
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-06-04 13:58:31 +0200

    update

--- src/components/Function/Request.vue
+++ src/components/Function/Request.vue
@@ -146,7 +146,7 @@ export default {
     computed: {
         routesOptions() {
             return this.plugin.routes.map(api => ({
-                label: api.Path,
+                label: `${api.Method} - ${api.Name}`,
                 value: api.Path,
             }));
         },
